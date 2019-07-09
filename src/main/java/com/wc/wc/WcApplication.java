e71a7bae0a4d476e90c626ae28003c312ece0182
chaojunhaha <1435941277@qq.com>
2019-07-09 14:41:07 +0800

    first commit

---
+++ src/main/java/com/wc/wc/WcApplication.java
@@ -0,0 +1,13 @@
+package com.wc.wc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WcApplication.class, args);
+    }
+
+}
